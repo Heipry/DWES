1266ff86679c27844c42ef3c64f4bf41ea5cbb2a
heipry <heipry@gmail.com>
2018-07-09 11:39:00 +0200

    Cambio espacios desde Netbeans

--- Proyecto1/procesa.php
+++ Proyecto1/procesa.php
@@ -1,5 +1,4 @@
 <?php
 echo 'Su nombre: '.$_POST['name'].' y ciclo: '.$_POST['ciclo'].' han sido recogidos';
 // $_POST y $_GET se pueden sustituir por $_REQUEST
-
 echo '<br>Nombre: '.$_REQUEST['name'].'<br>';
