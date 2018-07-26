0821db3b5f9caa5f7dd3615981cbe49dc71313b9
heipry <heipry@gmail.com>
2018-07-26 14:33:39 +0200

    Modificando ejercicio2

--- Unidad3/stock-tiendas.php
+++ Unidad3/stock-tiendas.php
@@ -18,14 +18,26 @@
 <body>
     <div id="encabezado">
         <h1>Ejercicio: Conjuntos de resultados en MySQLi</h1>
-        <form id="form_seleccion" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-            <select name="productos">
-                <?php 
+        <?php 
                 $dwes = new mysqli('localhost', 'dwes', 'abc123.', 'dwes');
                 $error = $dwes->connect_errno;
                 if ($error!=0) {
                     $mensaje = "Se produjo un error: ". $error. " | ". $dwes->connect_error;
                 }else{
+                    if (isset($_GET['id'])){
+                        $consultaC = $dwes->stmt_init();
+                        $uni = $_GET['stock'];
+                        $prod = $_GET['id'];
+                        $tienda = $_GET['tienda'];
+                        echo $uni.$tienda.$prod;
+                        $consultaC->prepare("UPDATE stock SET unidades = ? WHERE producto = \'?\'AND tienda =  ?");
+                        $consultaC->bind_param('isi', $uni,$prod,$tienda);
+                        $consultaC->close();
+                    }
+        ?>
+        <form id="form_seleccion" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+            <select name="productos">
+                <?php 
                     $consulta = 'SELECT distinct producto FROM stock';
                     $resul = $dwes->query($consulta, MYSQLI_USE_RESULT);
                     $producto= $resul->fetch_array();
@@ -44,8 +56,8 @@
     <div id="contenido">
         <h2>Stock del producto</h2>
     <?php
-        if (isset($_POST['productos'])) {
-            $producto = $_POST['productos'];
+        if (isset($_POST['productos']) || isset($_GET['id'])) {
+            $producto = (isset($_POST['productos'])) ? $_POST['productos'] : $_GET['id'];
             $consulta1 = 'SELECT cod, nombre FROM tienda';
             $tiendas = $dwes->query($consulta1);
             $tienda= $tiendas->fetch_array();
@@ -58,9 +70,10 @@
                 echo '<p>Cambiar stock a:';
             ?>
             <form id="form_action" action="<?php echo $_SERVER['PHP_SELF'];?>" method="get">
-            <input type="hidden" value="<?php  $producto ?>">
-            <input type="hidden" value="<?php  $$tienda[0] ?>">
-            <input type="number" value="<?php  $producto ?>">
+            <input type="hidden" value="<?php  echo $producto ?>" name='id'>
+            <input type="hidden" value="<?php  echo $tienda[0] ?>" name ='tienda'>
+            <input type="number" value="<?php  echo $stockNum ?>" name='stock'>
+            <button type="submit">Modificar stock</button> 
             </form>
             <?php     
                 $tienda= $tiendas->fetch_array();         
